55f97883a029c276e8b0fb29fd1d79ad1b9b0f05
Charlie McClung <charlie@cmr1.com>
2017-08-14 12:43:26 -0600

    Update docker entrypoint with new DB ENV vars

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,11 +2,7 @@
 
 set -e
 
-DB_HOST=$(echo $DATABASE_URL| cut -d'@' -f 2 | cut -d'/' -f 1);
-
-echo "DB HOST: $DB_HOST"
-
-./wait-for-it.sh ${DB_HOST} --strict --timeout=60
+./wait-for-it.sh ${DATABASE_HOST}:${DATABASE_PORT} --strict --timeout=60
 
 # bundle exec rake db:create
 # bundle exec rake db:migrate
